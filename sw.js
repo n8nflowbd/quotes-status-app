2f5b3e3e6fde035ad79c6be64ffec2e890f076ed
Bappa Goswami <goswamibappakk@gmail.com>
2026-03-14 01:06:27 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,32 @@
-const CACHE = 'quotes-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+// Cache version - change this to force update
+const CACHE = 'quotes-v3';
+
+// On install - delete old cache, cache new files
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./', './index.html']))
+  );
+});
+
+// On activate - delete ALL old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch - network first, then cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
